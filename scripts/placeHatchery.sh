cebd1e86501d9945eab8bdeeb0ff6141be27a678
twosevenska <twosevenska@gmail.com>
2017-01-25 16:57:47 +0000

    Updated placeHatchery so it can do update and actually work

--- scripts/placeHatchery.sh
+++ scripts/placeHatchery.sh
@@ -5,45 +5,69 @@ underline=$(tput smul)
 normal=$(tput sgr0)
 bldred=${bold}$(tput setaf 1) #  red
 
+BESPIN_SERVICES="Glass,Trident,Lobby,Petros,SwaggerUI,Focus,Blur,Cocktail,Zuul,Babel,Shinkansen"
+
 clear
 
 echo -e "${bldred}The Swarm prevails."
 echo -e "----------------------------"
 echo -e "!!!!!!! WARNING THIS SCRIPT MAY TAKE LONGER THAN YOU'D LIKE!${normal}"
 
-echo -e "Do you wish to ${underline}shutdown${normal} the bespin stack and associated services?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) clear;cd /repos/env/deploy;bespin kill;shared-services kill; break;;
-        No ) clear;;
-    esac
+cd /repos/env/deploy
+
+while :
+do
+	echo -e "Do you wish to ${underline}update${normal} the core bespin stack and shared services? (y/n)"
+	read yn
+	case $yn in
+		y)
+			bespin -s $BESPIN_SERVICES pull
+			shared-services pull
+			break
+			;;
+		n)
+			break
+			;;
+		*)
+			echo "Invalid option"
+			;;
+	esac
 done
 
-
-echo -e "Do you wish to ${underline}update${normal} the bespin stack and associated services?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) clear;bespin -s 'Glass Trident Lobby Petros SwaggerUI Focus Blur Cocktail Zuul Babel Shinkansen' pull;shared-services pull; break;;
-        No ) clear;;
-    esac
+while :
+do
+	echo -e "Do you wish to ${underline}shutdown all${normal} of the bespin stack and associated services? (y/n)"
+	read yn
+	case $yn in
+		y)
+			bespin kill
+			shared-services kill
+			break
+			;;
+		n)
+			break
+			;;
+		*)
+			echo "Invalid option"
+			;;
+	esac
 done
 
-
-echo -e "Do you wish to ${underline}start all${normal} of the bespin stack and associated services?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) shared-services up; bespin -s 'Glass Trident Lobby Petros SwaggerUI Focus Blur Cocktail Zuul Babel Shinkansen' up; break;;
-        No ) exit;;
-    esac
+while :
+do
+	echo -e "Do you wish to ${underline}start the core${normal} of the bespin stack and associated services? (y/n)"
+	read yn
+	case $yn in
+		y)
+			shared-services up
+			bespin -s $BESPIN_SERVICES up
+			break
+			;;
+		n)
+			break
+			;;
+		*)
+			echo "Invalid option"
+			;;
+	esac
 done
-
-
-echo -e "Do you wish to start ${underline}NflexUI ${normal}?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) bespin -s NflexUI up; break;;
-        No ) exit;;
-    esac
-done
-
-
