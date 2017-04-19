4dbddcbe3b35674b0f66e2df2a076f48b08cd702
goncalo.barroso <goncalo.barroso@headforwards.com>
2017-04-19 11:28:48 +0000

    Remove work stuff in order to make the repo public and only for hobby life

--- hf/scripts/spawnChangeling.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Run this to backup settings back to luggage. Make this if syslinks do not work.
-# !!!!!!!!!!!!!!!!!!!!! Not tested !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
---
+++ my/scripts/updateGo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+bold=$(tput bold)
+underline=$(tput smul)
+normal=$(tput sgr0)
+bldorange=${bold}$(tput setaf 202) #  red
+bldblue=${bold}$(tput setaf 045) #  blue
+
+GO_VERSION=1.8.1
+
+clear
+
+echo -e "${bldblue}Athena is online and ready to update the Gophers${normal}"
+
+while :
+do
+	echo -e "${bldblue}Do you wish to update to version ${bldorange}$GO_VERSION? (Y/n)${normal}"
+	read yn
+	case $yn in
+		n)
+      echo "${bldblue}Type the version you wish, followed by [ENTER]:${normal}"
+      read GO_VERSION
+			break
+			;;
+		*)
+      # Install GO
+      if [ -d "go$GO_VERSION.linux-amd64.tar.gz" ]; then
+        echo "${bldblue}Found old download, removing it${normal}"
+        rm -rf go$GO_VERSION.linux-amd64.tar.gz
+      fi
+      echo "${bldblue} Downloading go$GO_VERSION.linux-amd64.tar.gz${normal}"
+      wget –quiet https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
+      if [ -d "/usr/local/go" ]; then
+        echo "${bldblue}Removing old version${normal}"
+        sudo rm -rf /usr/local/go
+      fi
+      echo "${bldblue}Unpacking go language${normal}"
+      sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+      rm -rf go$GO_VERSION.linux-amd64.tar.gz
+			break
+			;;
+	esac
+done
