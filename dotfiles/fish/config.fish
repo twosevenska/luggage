830ff8f8874e74e92c9ebbd64355fd4a6915876b
twosevenska <twosevenska@gmail.com>
2018-07-19 14:02:46 +0100

    Update fish configs

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,7 +2,6 @@ source ~/luggage/dotfiles/fish/.basic.fish
 source ~/luggage/dotfiles/fish/.alias.fish
 source ~/luggage/dotfiles/fish/.env.fish
 
-# for things not checked into git..
-if test -e "~/.extra.fish";
-	source ~/.extra.fish
+if test -d ~/luggageHF
+    source /repos/luggageHF/dotfiles/fish/.extra.fish
 end
