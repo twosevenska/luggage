4324bf5f049b114bf31fa6508b833c97d279e25e
twosevenska <twosevenska@gmail.com>
2018-08-07 12:35:33 +0000

    Fix GoEnvs

--- dotfiles/fish/.env.fish
+++ dotfiles/fish/.env.fish
@@ -7,9 +7,10 @@ if test -e "$WORKSPACE/go"
     case Linux
         set -x -U GOROOT /usr/local/go
         set -x -U GOPATH $WORKSPACE/go
-        set -x -U GOBIN $GOPATH/bin 
-        set PATH $PATH $GOROOT/bin $GOBIN
+        set PATH $PATH $GOROOT/bin $GOPATH/bin 
     case Darwin
-        set -x -U GOROOT /usr/local/opt/go/libexec/bin
+        set -x -U GOROOT /usr/local/opt/go/libexec
+        set -x -U GOPATH $WORKSPACE/go
+        set PATH $PATH $GOROOT/bin $GOPATH/bin 
     end
 end
