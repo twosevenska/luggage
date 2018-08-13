c5c392b89bd72cec09d808ec2dd20f3f75ba3146
twosevenska <twosevenska@gmail.com>
2018-08-13 13:38:00 +0100

    Update fish configs for nvm (osx) and pyenv (linux)

--- dotfiles/fish/.env.fish
+++ dotfiles/fish/.env.fish
@@ -14,3 +14,19 @@ if test -e "$WORKSPACE/go"
         set PATH $PATH $GOROOT/bin $GOPATH/bin 
     end
 end
+
+# Pyenv variables
+if test -e "~/.pyenv"
+    switch (uname)
+    case Linux
+        set PATH "~/.pyenv/bin" $PATH
+        set -x PYENV_ROOT $HOME/.pyenv
+        set -x PATH $PYENV_ROOT/bin $PATH
+        set -x PATH $HOME/.pyenv/shims $PATH
+    end
+end
+
+# NVM function
+function nvm
+    bass source /usr/local/opt/nvm/nvm.sh --no-use ';' nvm $argv
+end
