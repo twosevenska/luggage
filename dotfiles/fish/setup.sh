c2cb6938de0a2850e81086d8d6082b8cb2c63590
goncalo.barroso <goncalo.barroso@headforwards.com>
2018-10-22 12:02:22 +0100

    Add setup.sh for fish

---
+++ dotfiles/fish/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# General setup just to make things look pretty and nerdy
+bold=$(tput bold)
+underline=$(tput smul)
+normal=$(tput sgr0)
+bldred=${bold}$(tput setaf 1) #  red
+bldgreen=${bold}$(tput setaf 2) #  green
+
+# The actual meat of the script
+
+if [ -d ~/.config/fish ]; then
+    if [ -f ~/.config/fish/config.fish ]; then
+        cp ~/luggage/dotfiles/fish/config.fish ~/.config/fish/config.fish
+    else
+        ln -sfn ~/luggage/dotfiles/fish/config.fish ~/.config/fish/config.fish
+    fi
+
+    if [ -f ~/.config/fish/fishfile ]; then
+        cp ~/luggage/dotfiles/fish/fishfile ~/.config/fish/fishfile
+    else
+        ln -sfn ~/luggage/dotfiles/fish/fishfile ~/.config/fish/fishfile
+    fi
+else
+    echo -e "${bldgree}Fish does not seem to be ready!${normal}"
+fi
