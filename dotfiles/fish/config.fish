f99c404c5931989f63242981890660b5b71cc733
goncalo.barroso <goncalo.barroso@headforwards.com>
2018-10-22 11:57:57 +0100

    Add support for loading system specific configs

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,6 +2,12 @@ source ~/luggage/dotfiles/fish/.basic.fish
 source ~/luggage/dotfiles/fish/.alias.fish
 source ~/luggage/dotfiles/fish/.env.fish
 
+# Loads Work related extras if symlink for repo is found
 if test -d ~/luggageHF
     source ~/luggageHF/dotfiles/fish/.extra.fish
+end
+
+# Loads any system especific configs and potentially overwrite previous stuff
+if test -e ~/.extras.fish
+    source ~/.extra.fish
 end
