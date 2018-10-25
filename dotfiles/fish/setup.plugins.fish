6c0b59b82cec2b5da9238eb12ed661785ebc447c
goncalo.barroso <goncalo.barroso@headforwards.com>
2018-10-25 09:13:18 +0100

    Replace fish file with a fish script

---
+++ dotfiles/fish/setup.plugins.fish
@@ -0,0 +1,12 @@
+#! /usr/bin/fish
+
+fisher add oh-my-fish/plugin-bang-bang
+fisher add edc/bass
+fisher add oh-my-fish/plugin-brew
+fisher add barnybug/docker-fish-completion
+fisher add franciscolourenco/done
+fisher add oh-my-fish/plugin-extract
+fisher add oh-my-fish/plugin-osx
+fisher add fisherman/pipenv-fish
+fisher add fisherman/pyenv
+fisher add spentacular/fish-theme-sh
